5f8fbb848958039dc437ac152b647407d43ae65d
Daniel Shevchenko <daniel.shevchenko@gmail.com>
2014-04-12 20:08:39 -0700

    fixing formatting

--- tarofriends-java/TaroFriends.java
+++ tarofriends-java/TaroFriends.java
@@ -2,32 +2,32 @@ import java.util.Arrays;
 
 public class TaroFriends {
 
-	public int getNumber(int[] coordinates, int X) {
+  public int getNumber(int[] coordinates, int X) {
     Arrays.sort(coordinates);
 
     int N = coordinates.length;
-	  int r = Integer.MAX_VALUE;
+    int r = Integer.MAX_VALUE;
 
-	  for (int i = 0; i <= N; i++) {
-	    int min = Integer.MAX_VALUE;
-	    int max = Integer.MIN_VALUE;
+    for (int i = 0; i <= N; i++) {
+      int min = Integer.MAX_VALUE;
+      int max = Integer.MIN_VALUE;
 
-	    for (int j = 0; j < N; j++) {
-	      int newPos = coordinates[j];
-	      if (j >= i) {
+      for (int j = 0; j < N; j++) {
+        int newPos = coordinates[j];
+        if (j >= i) {
           newPos -= X;
-	      } else {
+        } else {
           newPos += X;
-	      }
+        }
 
-	      min = Math.min(min, newPos);
-	      max = Math.max(max, newPos);
-	    }
+        min = Math.min(min, newPos);
+        max = Math.max(max, newPos);
+      }
 
-	    r = Math.min(r, max - min);
-	  }
+      r = Math.min(r, max - min);
+    }
 
-		return r;
-	}
+    return r;
+  }
 
 }
